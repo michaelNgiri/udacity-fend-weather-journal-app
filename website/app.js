20d0d3fb58654857a64a936ca03844d9dcbda787
Michael Ngiri <michaelngiri@gmail.com>
2023-02-05 22:18:47 +0100

    add open API key

--- website/app.js
+++ website/app.js
@@ -1,4 +1,6 @@
 /* Global Variables */
+// Personal API Key for OpenWeatherMap API
+const apiKey = '117185f20d89f35255df3556e43f4626&units=imperial';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
