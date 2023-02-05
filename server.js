7484fbc778e7ffffc76459a1bd723a9e72e57561
Michael Ngiri <michaelngiri@gmail.com>
2023-02-05 22:15:55 +0100

    initialize the project

---
+++ server.js
@@ -0,0 +1,29 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Require Express to run server and routes
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+// Start up an instance of app
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors())
+
+// Cors for cross origin allowance
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+
+// Setup Server
+const PORT = 3001
+
+app.listen(PORT, function() {
+    console.log(`app running on port ${PORT}`)
+});
